cf882d500c1b1df3997402e7a13e1823826ef9ad
TheCong2004 <thecong2610@gmail.com>
2025-05-24 10:50:30 +0700

    thêm

--- baitapnhom1web/src/main/java/com/example/baitapnhom1web/controller/GroupController.java
+++ baitapnhom1web/src/main/java/com/example/baitapnhom1web/controller/GroupController.java
@@ -1,10 +1,12 @@
 package com.example.baitapnhom1web.controller;
 
 
+import com.example.baitapnhom1web.model.Group;
 import com.example.baitapnhom1web.repository.GroupRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GroupController {
@@ -20,4 +22,12 @@ public class GroupController {
         model.addAttribute("groups", groupRepository.findAll());
             return "groups"; // trỏ tới templates/groups.html
     }
+    @GetMapping("/groups/{id}")
+    public String getGroupDetail(@PathVariable Long id, Model model) {
+        Group group = groupRepository.findById(id).orElseThrow();
+        model.addAttribute("group", group);
+        return "detai"; // tương ứng với detail.html
+    }
+
+
 }
